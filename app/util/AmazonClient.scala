02eeb6ff57eb4adf410e5446b9ffa99492e1e857
patoarvizu <patricio@onswipe.com>
2014-10-31 12:35:53 -0400

    Change credential provider to one that looks in multiple places

--- app/util/AmazonClient.scala
+++ app/util/AmazonClient.scala
@@ -5,9 +5,10 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.sqs.AmazonSQSAsyncClient
 import com.amazonaws.services.autoscaling.AmazonAutoScalingAsyncClient
 import com.amazonaws.services.ec2.AmazonEC2AsyncClient
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 
 trait AmazonClient {
-    val credentials: AWSCredentials = new ProfileCredentialsProvider().getCredentials
+    val credentials: AWSCredentials = new DefaultAWSCredentialsProviderChain().getCredentials
     val sqsClient: AmazonSQSAsyncClient = new AmazonSQSAsyncClient(credentials)
     val asClient: AmazonAutoScalingAsyncClient = new AmazonAutoScalingAsyncClient(credentials)
     val ec2ClientAsync: AmazonEC2AsyncClient = new AmazonEC2AsyncClient(credentials)
